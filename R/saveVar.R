8ac049c2b87c7166920d9d14c33d9595c926188c
Jiaying Liao <jiaying.liao@qq.com>
2023-04-05 12:28:48 -0700

    add function saveVAr

--- R/saveVar.R
+++ R/saveVar.R
@@ -1,19 +1,20 @@
 #' Save the variable into file as .rds
-#'
+#' @importFrom here here
 #' @param var the variable to be saved
 #' @param name the expected name of variable, should followed by .rds
 #' @param out_dir the location of saved variable
 #'
 #' @return a message of saved variable information
-#'
+#' @export
 #' @examples
-#' saveVar(10,"num.rds","variable")
+#' #saveVar(10,"num.rds","saved")
 saveVar <- function(var, name, out_dir) {
-  if (!dir.exists(out_dir)) {
-    dir.create(out_dir)
+  full_dir <- here::here(out_dir)
+  if (!dir.exists(full_dir)) {
+    dir.create(full_dir)
   }
-  saveRDS(var, file = paste0(out_dir, "/", name))
-  print(paste(name, "saved to", out_dir))
+  saveRDS(var, file = paste0(full_dir, "/", name))
+  print(paste(name, "saved to", full_dir))
   return(paste(name, "saved to", out_dir)) #return for testing purpose
 }
 
--- tests/testthat/test-saveVar.R
+++ tests/testthat/test-saveVar.R
@@ -1,4 +1,4 @@
 test_that("successfully saved", {
-  expect_equal(saveVar(10,"num.rds","variable"),
-               "num.rds saved to variable")
+  expect_equal(saveVar(10,"num.rds","saved"),
+               "num.rds saved to saved")
 })
