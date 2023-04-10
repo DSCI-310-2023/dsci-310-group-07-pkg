9d1e9ff0b21951891b8d1ef6e611c1397bb0682d
Jiaying Liao <jiaying.liao@qq.com>
2023-04-09 21:03:28 -0700

    load data done

---
+++ R/automobile.R
@@ -0,0 +1,12 @@
+#' Automobile dataset
+#'
+#' A dataset containing information about automobiles
+#'
+#' @format A data frame with 205 rows and 26 variables:
+#'
+#' @name automobile
+#'
+#' @export
+#'
+#' @keywords datasets
+load("data/automobile.rda")
